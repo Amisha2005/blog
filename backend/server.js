d7fce351e687ebbc27621026c17bed582749bd01
AmeCodeHere <kt1740594@gmail.com>
2026-04-15 22:22:35 +0530

    changed the port

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const { Groq } = require("groq-sdk");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const connectDb = require("./utils/db");
 const seedLocalUsersIfNeeded = require("./utils/seedUsers");
 require("dotenv").config();
@@ -374,7 +374,7 @@ connectDb()
     // app.listen(PORT, () => {
     //   console.log(`Server running on http://localhost:${PORT}`);
     // });
-    const PORT = process.env.PORT || 5000;
+    
     app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on port ${PORT}`);
     });
